f60efe63d3919c1a83307ef6a70d8ecfe644ca68
Bednarooo <236502@edu.p.lodz.pl>
2023-06-18 15:58:09 +0200

    initial commit

---
+++ src/main/java/com/example/itlectures/ItLecturesApplication.java
@@ -0,0 +1,13 @@
+package com.example.itlectures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItLecturesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItLecturesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/itlectures/ItLecturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itlectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItLecturesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
